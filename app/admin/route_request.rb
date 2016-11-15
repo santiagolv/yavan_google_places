c2cf35b71c8750d31ec832c64749eebea15dfd0f
Raghu Betina <raghu@firstdraft.com>
2016-11-15 23:20:56 +0000

    Whitelist columns in RouteRequest for ActiveAdmin

--- app/admin/route_request.rb
+++ app/admin/route_request.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register RouteRequest do
 
+ permit_params :origin_query, :origin_city, :origin_place, :origin_google_id, :destination_query, :destination_city, :destination_place, :destination_google_id, :destination_arrival_date_time, :max_time_in_advance, :user_id, :origin_google_suggested_departure_time
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
