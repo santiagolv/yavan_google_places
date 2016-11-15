5f5164fa28ee336d3830350d0bac11ab8ed4eb6e
Raghu Betina <raghu@firstdraft.com>
2016-11-15 23:21:01 +0000

    Whitelist columns in RoutePooling for ActiveAdmin

--- app/admin/route_pooling.rb
+++ app/admin/route_pooling.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register RoutePooling do
 
+ permit_params :arrival_date_time, :origin_place, :origin_city, :origin_google_id, :destination_place, :destination_city, :user_id, :route_request_id, :passengers_pooled, :average_buffer_time, :google_suggested_departure_time, :confirmed_route
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
