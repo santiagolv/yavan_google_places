a42870e53d4508049b355e4b0ec4df9cab2b154c
Raghu Betina <raghu@firstdraft.com>
2016-11-15 23:19:52 +0000

    Direct association between route_poolings and confirmed_passengers

--- app/models/confirmed_passenger.rb
+++ app/models/confirmed_passenger.rb
@@ -1,6 +1,9 @@
 class ConfirmedPassenger < ApplicationRecord
   # Direct associations
 
+  belongs_to :route_pool,
+             :class_name => "RoutePooling"
+
   belongs_to :user
 
   # Indirect associations
--- app/models/route_pooling.rb
+++ app/models/route_pooling.rb
@@ -1,6 +1,10 @@
 class RoutePooling < ApplicationRecord
   # Direct associations
 
+  has_many   :confirmed_passengers,
+             :foreign_key => "route_pool_id",
+             :dependent => :destroy
+
   belongs_to :route_request
 
   # Indirect associations
