4f65d161a692a78c60565874a260af5a1f91750c
Raghu Betina <raghu@firstdraft.com>
2016-11-15 23:19:52 +0000

    Direct association between route_requests and route_poolings

--- app/models/route_pooling.rb
+++ app/models/route_pooling.rb
@@ -1,6 +1,8 @@
 class RoutePooling < ApplicationRecord
   # Direct associations
 
+  belongs_to :route_request
+
   # Indirect associations
 
   # Validations
--- app/models/route_request.rb
+++ app/models/route_request.rb
@@ -1,6 +1,9 @@
 class RouteRequest < ApplicationRecord
   # Direct associations
 
+  has_one    :route_pooling,
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
