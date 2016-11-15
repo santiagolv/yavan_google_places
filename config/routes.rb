7f169d8eb86901305b6165cc2a785f196977bf2d
Raghu Betina <raghu@firstdraft.com>
2016-11-15 23:20:46 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "route_requests#index"
   # Routes for the Confirmed_passenger resource:
   # CREATE
